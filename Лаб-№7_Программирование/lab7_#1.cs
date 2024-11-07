8f9395a8fcfaefbd500395720951141a8a263393
qwerty-mod <slavaynk123@gmail.com>
2024-11-07 21:59:24 +0600

    Add files via upload

---
+++ Лаб-№7_Программирование/lab7_#1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1. Дан ступенчатый массив, размерностью 4 (4 ссылки на массивы)
+            // задать размерность и элементы каждого массива
+
+            // для каждого массива определить количество четных и нечетных элементов
+
+            int[][] mass = new int[4][];
+
+            for (int x = 0; x < 4; x++)
+            {
+                Console.Write("Введите количество элементов текущего массива: ");
+                int n = int.Parse(Console.ReadLine());
+                mass[x] = new int[n];
+
+                Console.WriteLine("Введите элементы массива");
+                for (int i = 0; i < n; i++)
+                {
+                    mass[x][i] = int.Parse(Console.ReadLine());
+                }
+
+
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("ИТОГ: ");
+            for (int i =0; i < 4; i++)
+            {
+                int k_chet = 0;
+                int k_nechet = 0;
+                for (int j = 0; j < mass[i].Length; j++)
+                {
+                    if (mass[i][j] % 2 == 0)
+                    {
+                        k_chet++;
+                    }
+                    else
+                    {
+                        k_nechet++;
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Строка {i+1}. Количество чётных: " + k_chet + " /// " +"Количество нечётных: " + k_nechet);
+                
+            }
+
+
+
+
+
+
+
+
+
+            Console.WriteLine();
+
+            //
+            Console.WriteLine("МАССИВ");
+            Console.WriteLine();
+            for (int x = 0;x < 4; x++)
+            {
+                
+                for (int y = 0; y < mass[x].Length ; y++)
+                {
+                    Console.Write(mass[x][y] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
---
+++ Лаб-№7_Программирование/lab7_#2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 2. дан ступенчатый массив размерностью 4.
+            // для каждой строки определить положение элемента
+            // значение которого больше чем сумма остальных элементов строки,
+            // если такого нет выдать сообщение указав номер строки массива
+            
+            int[][] mass = new int[4][];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Введите количество элементов {i + 1} строки: ");
+                int n = int.Parse(Console.ReadLine());
+                mass[i] = new int[n];
+                Console.WriteLine("Введите элементы строки через Enter");
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                
+                int suma = 0;
+                for (int j = 0;j < mass[i].Length; j++)
+                {
+                    suma += mass[i][j];
+                    
+                    
+                }
+                
+                int uslovie = 0;
+                string posicion = "";
+                int EL_proverka = 0;
+                for (int j = 0; j < mass[i].Length; j++)
+                {
+                    int EL = mass[i][j];
+                    
+                    if (suma - EL < EL)
+                    {
+                        
+                        uslovie = EL;
+                        posicion = $"({i},{j})";
+                        EL_proverka = EL;
+                    }
+                    
+                }
+                if (uslovie != 0)
+                {
+                    Console.WriteLine($"В строке {i+1} элементом является {EL_proverka}, его позиция в массиве {posicion}");
+                }
+                else
+                {
+                    Console.WriteLine($"В стркое {i+1} нет подхоядщих элементов"); 
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("МАССИВ");
+            Console.WriteLine();
+
+            for (int i = 0;i < 4; i++)
+            {
+                for (int j = 0;j < mass[i].Length; j++)
+                {
+                    Console.Write(mass[i][j] + " ");
+                }
+                Console.WriteLine() ;
+            }
+
+        }
+    }
+}
