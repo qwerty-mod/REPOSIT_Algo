e04b5410fe7bf9349bf1953b38afe9f996518d8e
qwerty-mod <slavaynk123@gmail.com>
2024-11-22 13:00:46 +0600

    Add files via upload

---
+++ Лаб-№9_Программирование/lab9_#1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // На вход дается набор строк, признак окончания ввода строк - пустая строка. 
+            // Задание 1.Определить количество строк, в которых присутствуют сочетания a*b, где * -любой символ.
+
+            int itogoviy_count = 0;
+            int counter = 0;
+            int i = 20;
+            Console.WriteLine("Введите строки");
+            while (i > 0)
+            {
+                string s = Console.ReadLine();
+                if (s == "")
+                {
+                    break;
+                }
+                
+                
+                // ПРОВЕРКА НА A*B
+                else
+                {
+                    char[] mass = new char[s.Length];
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        mass[j] = s[j] ;
+                        
+                    }
+                    for (int j = 0; j < s.Length - 2; j++)
+                    {
+                        if (mass[j] == 'a' || mass[j + 2] == 'b')
+                        {
+                            counter += 1;
+                        }
+                    }
+                }
+                if (counter != 0)
+                {
+                    itogoviy_count += 1;
+                }
+                counter = 0;
+            }
+            Console.WriteLine("Количество строк подходящих под условие: " + itogoviy_count);
+
+        }
+    }
+}
---
+++ Лаб-№9_Программирование/lab9_#2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab9__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //На вход дается набор строк, признак окончания ввода строк - пустая строка.
+            //Задание 2. Определить максимальную длину подстроки в каждой строе, 
+            //состоящей из сочетания abc, при этом необязательно присутствуют все три символа.
+            //Например, подстрока abcabcabc и abca тоже считаются.
+
+            Console.WriteLine("Введите строки (пустая строка для завершения):");
+            int maxLength = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) break;
+
+                int currentLength = 0;
+                char expectedChar = 'a'; // Начинаем с символа 'a'
+
+                foreach (char c in line)
+                {
+                    if (c == expectedChar)
+                    {
+                        currentLength++;
+
+                        // Переходим к следующему символу
+                        if (expectedChar == 'a') expectedChar = 'b';
+                        else if (expectedChar == 'b') expectedChar = 'c';
+                        else if (expectedChar == 'c') expectedChar = 'a'; // Цикл повторяется
+                    }
+                    else if (c == 'a') // Если порядок нарушен, но видим 'a', начинаем заново
+                    {
+                        currentLength = 1;
+                        expectedChar = 'b';
+                    }
+                    else
+                    {
+                        currentLength = 0;
+                        expectedChar = 'a';
+                    }
+
+                    // Обновляем максимальную длину
+                    if (currentLength > maxLength)
+                        maxLength = currentLength;
+                }
+            }
+
+            Console.WriteLine($"Максимальная длина подстроки: {maxLength}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
