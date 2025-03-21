b85316c82899e98cec35eeb86422e03dd5a47584
qwerty-mod <slavaynk123@gmail.com>
2025-03-21 19:59:28 +0600

    Add files via upload

---
+++ 2 СЕМЕСТР АиП/sem2_lab#8/sem2_lab#8_zad#1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+
+class Tochka
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Tochka(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Dvigat(int dx, int dy)
+    {
+        X += dx;
+        Y += dy;
+    }
+}
+
+class Pole 
+{
+    private int minX, maxX, minY, maxY;
+    private Random randik = new Random();
+    public Tochka Toch { get; private set; }
+
+    public Pole(int minX, int maxX, int minY, int maxY, int startX, int startY)
+    {
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        Toch = new Tochka(startX, startY);
+    }
+
+    public void DvigatTochku()
+    {
+        Toch.Dvigat(randik.Next(-1, 2), randik.Next(-1, 2));
+        Proverka_Granizi();
+    }
+
+    private void Proverka_Granizi()
+    {
+        if (Toch.X < minX || Toch.X > maxX || Toch.Y < minY || Toch.Y > maxY)
+        {
+            Console.WriteLine($"Точка за границей! ({Toch.X}, {Toch.Y})");
+        }
+        else
+        {
+            Console.WriteLine($"Точка в пределах поля ({Toch.X}, {Toch.Y})");
+        }
+    }
+}
+
+class Programma
+{
+    static void Main()
+    {
+        Pole pole = new Pole(0, 5, 0, 5, 2, 2);  // поле размером 5 на 5 ( квадрат где левый нижний угол {0;0}, а правый верхний {5,5})
+
+        for (int i = 0; i < 20; i++)
+        {
+            pole.DvigatTochku();
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
+
+
---
+++ 2 СЕМЕСТР АиП/sem2_lab#8/sem2_lab#8_zad#2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Loshad
+{
+    public string Imya { get; }
+    public int X { get; private set; }
+    private Random rand;
+    private int minSpeed;
+    private int maxSpeed;
+
+    public Loshad(string imya, int minSkorost, int maxSkorost)
+    {
+        Imya = imya;
+        X = 0;
+        rand = new Random();
+        this.minSpeed = minSkorost;
+        this.maxSpeed = maxSkorost;
+    }
+
+    public void Begi()
+    {
+        X += rand.Next(minSpeed, maxSpeed + 1); // Разный диапазон скоростей для каждой лошади
+    }
+}
+
+class Gonka
+{
+    private Loshad[] loshadi;
+    private int finish;
+    private Random rand = new Random();
+
+    public Gonka(string[] imena, int finish)
+    {
+        this.finish = finish;
+        loshadi = new Loshad[imena.Length];
+        for (int i = 0; i < imena.Length; i++)
+        {
+            int minSkorost = rand.Next(1, 4);
+            int maxSkorost = rand.Next(minSkorost + 1, 7);
+            loshadi[i] = new Loshad(imena[i], minSkorost, maxSkorost);
+        }
+    }
+
+    public void Start()
+    {
+        bool estPobeditel = false;
+        while (!estPobeditel)
+        {
+            foreach (var loshad in loshadi)
+            {
+                loshad.Begi();
+                Console.WriteLine($"{loshad.Imya} begit: {loshad.X}");
+
+                if (loshad.X >= finish)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{loshad.Imya} победила!");
+                    Console.WriteLine();
+                    estPobeditel = true;
+                    break;
+                }
+            }
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
+
+class Programma
+{
+    static void Main()
+    {
+        string[] imena = { "Бурка", "Маквин", "Усейн Болт" };
+        Gonka gonka = new Gonka(imena, 20);
+        gonka.Start();
+    }
+}
+
+
+
+
+
