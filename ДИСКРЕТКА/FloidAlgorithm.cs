04b605db36048c8b38d3f85ee1a7b27134070d05
qwerty-mod <slavaynk123@gmail.com>
2025-04-06 15:26:19 +0600

    Add files via upload

---
+++ ДИСКРЕТКА/FloidAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FloidAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[,] test = {
+                { 0, 3, int.MaxValue, 7 },
+                { 8, 0, 2, int.MaxValue },
+                { 5, int.MaxValue, 0, 1 },
+                { 2, int.MaxValue, int.MaxValue, 0 }
+            };
+
+            
+            FloidAlgorithm(test);
+        }
+
+        static void FloidAlgorithm(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int[,] resMatrix = new int[n, n];
+
+            // Инициализация результата
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        resMatrix[i, j] = 0;
+                    }
+                    else
+                    {
+                        resMatrix[i, j] = matrix[i, j];
+                    }
+                }
+            }
+
+            // Алгоритм Флойда
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (resMatrix[i, k] != int.MaxValue && resMatrix[k, j] != int.MaxValue && resMatrix[i, k] + resMatrix[k, j] < resMatrix[i, j])
+                        {
+                            resMatrix[i, j] = resMatrix[i, k] + resMatrix[k, j];
+                        }
+                    }
+                }
+            }
+
+            // Вывод результата
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Из вершины {i}: ");
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(resMatrix[i, j] == int.MaxValue ? "INF " : resMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
