9af86d1e2ef73074425b41329a2b0c0ca4a8fc78
qwerty-mod <slavaynk123@gmail.com>
2025-04-07 16:31:30 +0600

    Add files via upload

---
+++ 2 СЕМЕСТР АиП/sem2_lab#9/sem2_lab#9_zad#1 .cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Введите первое число: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Лямбда-выражения для операций
+        Func<double, double, double> plus = (x, y) => x + y;
+        Func<double, double, double> minus = (x, y) => x - y;
+        Func<double, double, double> umnozh = (x, y) => x * y;
+        Func<double, double, double> delit = (x, y) => y != 0 ? x / y : 0; // Деление на 0 — вернёт 0
+
+        
+        for (int i = 0; i < 1; i++)
+        {
+            Console.WriteLine($"Сложение: {plus(a, b)}");
+            Console.WriteLine($"Вычитание: {minus(a, b)}");
+            Console.WriteLine($"Умножение: {umnozh(a, b)}");
+
+            if (b == 0)
+                Console.WriteLine("Деление: Ошибка (деление на 0)");
+            else
+                Console.WriteLine($"Деление: {delit(a, b)}");
+        }
+    }
+}
+
---
+++ 2 СЕМЕСТР АиП/sem2_lab#9/sem2_lab#9_zad#2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> spisokSlov = new List<string>
+        {
+            "море", "гора", "молоко", "лес", "машина", "река", "мята"
+        };
+
+        // Лямбда-выражение для отбора слов на "м"
+        var tolkoMslova = spisokSlov.Where(slovo => slovo.StartsWith("м")).ToList();
+
+        
+        Console.WriteLine("Слова которые начинаются на букву 'м' :");
+        Console.WriteLine();
+        foreach (var slovich in tolkoMslova)
+        {
+            Console.WriteLine(slovich);
+        }
+        Console.WriteLine();
+    }
+}
