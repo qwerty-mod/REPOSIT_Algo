66367f73ad7377402190917497d1b05dcfc6fa54
qwerty-mod <slavaynk123@gmail.com>
2025-05-22 00:17:02 +0600

    Add files via upload

---
+++ 2 СЕМЕСТР АиП/sem2_lab#16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Кол-во элементов: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // "Указатель" на массив 
+        int[] El = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            El[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+        int countDivBy2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (El[i] % 2 == 0)
+                countDivBy2++;
+        }
+
+        Console.WriteLine($"Количество элементов, кратных двум: {countDivBy2}");
+    }
+}
+
