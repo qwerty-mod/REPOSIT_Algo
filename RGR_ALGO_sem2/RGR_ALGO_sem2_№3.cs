d917a67bf8db38f11ba17fb47eb91890b00b341b
qwerty-mod <slavaynk123@gmail.com>
2025-06-02 15:58:55 +0600

    Add files via upload

---
+++ RGR_ALGO_sem2/RGR_ALGO_sem2_№3.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class P
+{
+    static Dictionary<string, Queue<string>> m = new Dictionary<string, Queue<string>>();
+    static HashSet<string> u = new HashSet<string>();
+    static List<int> l = new List<int>();
+
+    static void Main()
+    {
+        var p = @"C:\Users\user\Desktop\input.txt";
+        var c = File.ReadAllLines(p).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+
+        foreach (var t in c)
+        {
+            var k = t[0] + "_" + t[t.Length - 1];
+            if (!m.ContainsKey(k)) m[k] = new Queue<string>();
+            m[k].Enqueue(t);
+        }
+
+        while (u.Count < c.Count)
+        {
+            List<string> mc = null;
+
+            foreach (var v in m)
+            {
+                foreach (var t in v.Value)
+                {
+                    if (u.Contains(t)) continue;
+
+                    var s = new HashSet<string>();
+                    var r = new List<string>();
+                    List<string> f;
+
+                    F(t, s, r, out f);
+
+                    if (f != null && (mc == null || f.Count > mc.Count)) mc = f;
+                    break;
+                }
+            }
+
+            if (mc != null)
+            {
+                foreach (var t in mc) u.Add(t);
+                l.Add(mc.Count);
+            }
+        }
+
+        Console.WriteLine(l.Count);
+        foreach (var x in l) Console.WriteLine(x);
+    }
+
+    static void F(string t, HashSet<string> s, List<string> r, out List<string> b)
+    {
+        r.Add(t);
+        s.Add(t);
+        b = null;
+
+        char e = t[t.Length - 1];
+
+        for (char d = 'a'; d <= 'z'; d++)
+        {
+            string k = e + "_" + d;
+            if (!m.ContainsKey(k)) continue;
+
+            foreach (var n in m[k])
+            {
+                if (u.Contains(n) || s.Contains(n)) continue;
+
+                List<string> z;
+                F(n, s, r, out z);
+
+                if (z != null && (b == null || z.Count > b.Count)) b = new List<string>(z);
+            }
+        }
+
+        if (r.Count >= 2 && r[0][0] == r[r.Count - 1][r[r.Count - 1].Length - 1])
+        {
+            if (b == null || r.Count > b.Count) b = new List<string>(r);
+        }
+
+        r.RemoveAt(r.Count - 1);
+        s.Remove(t);
+    }
+}
---
+++ RGR_ALGO_sem2/RGR_ALGO_sem2_№4.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class X
+{
+    static int[,] G;
+    static Queue<string[]> Q = new Queue<string[]>();
+
+    static void Main()
+    {
+        var L = File.ReadAllLines(@"C:\Users\user\Desktop\input.txt");
+        var D = L[0].Split();
+        int W = int.Parse(D[0]), H = int.Parse(D[1]);
+        G = new int[H, W];
+
+        P(L[1], 0, 0, W, H, L);
+
+        for (int y = 0; y < H; y++)
+        {
+            for (int x = 0; x < W; x++)
+            {
+                Console.Write(G[y, x] + (x < W - 1 ? " " : ""));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void P(string s, int x, int y, int w, int h, string[] L)
+    {
+        if (s == "Z")
+        {
+            Q.Enqueue(new string[] { x.ToString(), y.ToString(), w.ToString(), h.ToString() });
+            R(L);
+        }
+        else
+        {
+            F(x, y, w, h, int.Parse(s));
+        }
+    }
+
+    static void R(string[] L)
+    {
+        int i = 2;
+
+        while (Q.Count > 0)
+        {
+            var c = Q.Dequeue();
+            int x = int.Parse(c[0]), y = int.Parse(c[1]), w = int.Parse(c[2]), h = int.Parse(c[3]);
+
+            if (i >= L.Length) continue;
+
+            var p = L[i++].Split();
+            T(p[0], x, y, w / 2 + w % 2, h / 2 + h % 2, L);
+            T(p[1], x + w / 2 + w % 2, y, w / 2, h / 2 + h % 2, L);
+            T(p[2], x, y + h / 2 + h % 2, w / 2 + w % 2, h / 2, L);
+            T(p[3], x + w / 2 + w % 2, y + h / 2 + h % 2, w / 2, h / 2, L);
+        }
+    }
+
+    static void T(string p, int x, int y, int w, int h, string[] L)
+    {
+        if (p == "-") return;
+
+        if (p == "Z")
+        {
+            Q.Enqueue(new string[] { x.ToString(), y.ToString(), w.ToString(), h.ToString() });
+        }
+        else
+        {
+            F(x, y, w, h, int.Parse(p));
+        }
+    }
+
+    static void F(int x, int y, int w, int h, int c)
+    {
+        for (int i = y; i < y + h; i++)
+        {
+            for (int j = x; j < x + w; j++)
+            {
+                G[i, j] = c;
+            }
+        }
+    }
+}
